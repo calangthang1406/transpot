8197d97cbde7252e6f10717a94465d13eb9cceba
calangthang1406 <nguyentrananhkhoa.wf@gmail.com>
2022-12-12 15:33:31 +0700

    Update code

---
+++ pTienVaPThuc.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class pTienVaPThuc {
+
+    // private String ptGiaoHang;
+    private String phuongTien;
+
+    public pTienVaPThuc() {
+    }
+
+    public pTienVaPThuc(/*String ptGiaoHang*/String phuongTien) {
+        //this.ptGiaoHang = ptGiaoHang;
+        this.phuongTien = phuongTien;
+    }
+
+    /*public String getPtGiaoHang() {
+        return ptGiaoHang;
+    }
+
+    public void setPtGiaoHang(String ptGiaoHang) {
+        this.ptGiaoHang = ptGiaoHang;
+    }
+     */
+    public String getPhuongTien() {
+        return phuongTien;
+    }
+
+    public void setPhuongTien(String phuongTien) {
+        this.phuongTien = phuongTien;
+    }
+/* PHƯƠNG THỨC */
+    /* ( xuất phương tiện giá hàng )
+     nếu cùng miền xuất " phương tiện: xe tải " nếu khác miền xuất " phuuwogn tiện : máy bay " */
+    
+}
